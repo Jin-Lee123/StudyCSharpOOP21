3eab36d562d94a87c5f55809bf5ee10c599dc474
Jin-Lee123 <wls4310@koreatech.ac.kr>
2021-05-04 12:47:01 +0900

    20210504_오전 커밋

---
+++ OOPSolution/ClassTestApp/Cat.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTestApp
+{
+    class Cat : Animal
+    {
+        /*public string Name { get; set; }*/
+        public string Color { get; set; }
+        public int Age { get; set; }
+
+        public Cat() { }
+
+        public Cat(string name, string color, int age)
+        {
+            //Initalization
+            this.Name = name;
+            this.Color = color;
+            this.Age = age;
+        }
+        public Cat(string name,string color)
+        {
+            this.Name = name;
+            this.Color = color;
+            
+        }
+        public Cat(string color, int age)
+        {
+           
+            this.Color = color;
+            this.Age = age;
+
+        }
+
+
+        public void Meow() { Console.WriteLine($"{this.Color} {this.Name}, 야옹!"); }
+
+        public override void Sleep()
+        {
+            base.Sleep();
+            //부모의 Sleep() 실행
+            Console.WriteLine($"{this.Color} 고양이 {this.Name}이(가) zz잡니다."); 
+            //이후 자식 클래스의 Sleep  내용 실행 (재정의:부모일하고 나머지 더 플러스 알파 느낌)
+        }
+    }
+}
---
+++ OOPSolution/ClassTestApp/MainApp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassTestApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("고양이 객체 생성");
+            Cat kitty = new Cat("키티",3);
+            //kitty.Name = "키티"; 
+            kitty.Age = 3;
+            kitty.Color = "햐얀색";
+            kitty.Meow();
+
+            /*Cat nero = new Cat
+            {
+                Name = "네로",
+                Age = 12,
+                Color = "검정색"
+            };*/
+            Cat nero = new Cat("네로", "검은", 12);
+            nero.Meow();
+
+            Cat mimi = new Cat("미미", "노랑색", 3);
+            mimi.Meow();
+
+            Cat coco = new Cat("코코", "얼룩이");
+            coco.Age = 2;
+            coco.Meow();
+
+            Cat noname = new Cat("야옹이","은색", 10);
+            //noname.Name = "야옹이";
+            noname.Meow();
+            noname.Sleep();
+
+            var list = (First : "Cat", Second : "Dog", Third : "Pig",55);
+            Console.WriteLine($"튜플 첫번째 : {list.First}");
+            Console.WriteLine($"튜플 두번째 : {list.Second}");
+            Console.WriteLine($"튜플 세번째 : {list.Third}");
+            Console.WriteLine($"튜플 네번째 : {list.Item4}");
+        }
+    }
+}
