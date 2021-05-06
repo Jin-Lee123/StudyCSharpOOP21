460e0d2652fb9f6d0b7bbd877046fdc35df3b349
Jin-Lee123 <wls4310@koreatech.ac.kr>
2021-05-06 12:47:24 +0900

    20210506 오전커밋

---
+++ OOPSolution/PropertyTestApp/Dog.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PropertyTestApp
+{
+    class Dog
+    {
+        private int age;
+        private string name;
+        public string Name { get; set; } = "멍멍이";  //초기값 설정
+        public string Color { get; set; } = "누런색"; //초기값 설정
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value<0)
+                {
+                    this.age = 1;
+                }
+                else if (value>15)
+                {
+                    this.age = 15;
+                }
+                else
+                {
+                    this.age = value;
+                }
+                age = value;
+            }
+        }
+
+        /*public string Getname() { return this.name; }
+        public void Setname(string name) { this.name=name; }
+       
+        public string GetAge() { return $"{this.age}"; }  //값을 사용
+        public void SetAge(int age)
+        {
+            if (age < 0)
+            {
+                this.age = 1;
+            }
+            else if (age > 15)
+            {
+                this.age = 15;
+            }
+            else
+            {
+                this.age = age;
+            }
+        } //값을 설정*/
+
+
+
+    }
+}
---
+++ OOPSolution/PropertyTestApp/MainApp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PropertyTestApp
+{
+    class MainApp
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("강아지 객체 생성");
+            Dog baekgoo = new Dog();
+            baekgoo.Name = "백구";
+            //baekgoo.SetAge(1000);  //5살
+
+            //Console.WriteLine($"백구의 나이는 {baekgoo.GetAge()}살");
+            baekgoo.Age = 25000;
+            Console.WriteLine($"{baekgoo.Name}의 나이는 {baekgoo.Age}살 ");
+
+            Dog streetDog = new Dog();
+            Console.WriteLine($"{streetDog.Name}는  {streetDog.Color} 입니다.");
+
+            Dog dog1 = new Dog();
+            dog1.Name = "황구";
+            dog1.Color = "노랑";
+            dog1.Age = 10;
+
+            Dog dog2 = new()
+            {
+                Name = "깜장이",
+                Color = "검정",
+                Age = 5
+            };
+
+            var myInstance = new { Name = "성명권", Age = 44 };
+            Console.WriteLine(myInstance.Name);
+            Console.WriteLine(myInstance.Age);
+        }
+    }
+}
